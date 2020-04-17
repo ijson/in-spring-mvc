7ad0495c9f40dee0459505b26f78b74a3827f44e
cuiyongxu <cuiyongxu@gmail.com>
2020-04-18 03:08:26 +0800

    a

--- src/main/java/com/ijson/controller/DefaultController.java
+++ src/main/java/com/ijson/controller/DefaultController.java
@@ -2,13 +2,22 @@ package com.ijson.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
 
-    public ModelAndView index() {
-        return new ModelAndView("index");
+    @RequestMapping("/default/buy")
+    public ModelAndView buy() {
+        ModelAndView modelAndView = new ModelAndView("/default/buy");
+        modelAndView.addObject("total", 10000);
+        return modelAndView;
+    }
+
+    @RequestMapping("/buy")
+    public ModelAndView buy2() {
+        ModelAndView modelAndView = new ModelAndView("/buy2");
+        modelAndView.addObject("total", 10000);
+        return modelAndView;
     }
 }
